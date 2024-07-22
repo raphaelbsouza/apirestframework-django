d4496fd8a8be5d4b0c89418317b1e3dbbcfa844a
Raphael Souza <raphaelstc@msn.com>
2024-07-22 08:58:32 -0300

    update repositor api rest framework django

---
+++ api_todo/app/urls.py
@@ -0,0 +1,10 @@
+#from django.contrib import admin
+from django.urls import path
+
+from app.views import todo_list,todo_detail_change_and_delete
+
+urlpatterns = [
+#   path("admin/", admin.site.urls),
+    path("", todo_list),
+    path("<int:pk>/", todo_detail_change_and_delete),
+]
