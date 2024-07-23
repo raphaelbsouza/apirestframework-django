2f11ed70612c1434c556183a123d2389f003302d
Raphael Souza <raphaelstc@msn.com>
2024-07-23 00:55:16 -0300

    update repositor api rest framework django

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,10 +1,8 @@
 #from django.contrib import admin
 from django.urls import path
+from app.views import TodoViewSet
+from rest_framework.routers import DefaultRouter
 
-from app.views import todo_list,todo_detail_change_and_delete
-
-urlpatterns = [
-#   path("admin/", admin.site.urls),
-    path("", todo_list),
-    path("<int:pk>/", todo_detail_change_and_delete),
-]
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+urlpatterns = router.urls
